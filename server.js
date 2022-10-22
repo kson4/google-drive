9a655d368dbea73b7c849bdf07a0764629709b56
kson4 <davidsinternetmail@gmail.com>
2022-10-22 11:19:46 -0700

    top nav markup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const ejs = require("ejs");
+
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.send("hi");
+  res.render("index");
 });
 
 app.listen(5000, () => {
