c6fbca59c9143fa61fd524cded55a9b3d726977a
kson4 <davidsinternetmail@gmail.com>
2022-10-23 12:58:36 -0700

    route file to textfile

--- server.js
+++ server.js
@@ -8,17 +8,27 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const documents = [];
+let id = 1;
 // module.export = { documents };
 
 app.get("/", (req, res) => {
   res.render("index", { documents: documents });
 });
 app.post("/index", (req, res) => {
+  const document = {
+    id: id,
+    type: req.body.document,
+    title: "new document",
+  };
+  id++;
   // documents.push(req.body.newDocument);
-  documents.push(req.body.document);
+  documents.push(document);
   console.log(req.body);
   res.redirect("/");
   console.log(documents);
+});
+app.get("/:id", (req, res) => {
+  res.render("document");
 });
 
 app.listen(5001, () => {
